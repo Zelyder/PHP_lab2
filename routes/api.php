acd2e2448926c4cebe08176ea49cc32f1268bc1c
Zelyder <57kaa@mail.ru>
2022-05-11 19:24:06 +0300

    Added CustomerController

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('customers', 'App\Http\Controllers\CustomerController@showAll');
+
+Route::get('customers/{customer}', 'App\Http\Controllers\CustomerController@show');
