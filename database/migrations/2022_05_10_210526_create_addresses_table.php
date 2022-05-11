99583967d02cacc7846748e6a90233579dfbe096
Zelyder <57kaa@mail.ru>
2022-05-11 04:19:49 +0300

    Complete task 2

---
+++ database/migrations/2022_05_10_210526_create_addresses_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->id();
+            $table->string('address');
+            $table->string('city');
+            $table->string('street');
+            $table->string('house');
+            $table->string('floor');
+            $table->string('flat');
+            $table->string('code');
+            $table->integer('customer_id');
+            $table->foreign('customer_id')
+                ->references('id')->on('customers')
+                ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('addresses');
+    }
+};
